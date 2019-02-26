9285f71407069b3a92e4b3f20ec37cfc98216971
ding1051416875 <1051416875@qq.com>
2019-02-26 15:34:27 +0800

    Initial Commit

---
+++ FrameWork/FrameWork.h
@@ -0,0 +1,19 @@
+//
+//  FrameWork.h
+//  FrameWork
+//
+//  Created by ding on 2019/2/26.
+//  Copyright © 2019年 dingXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameWork.
+FOUNDATION_EXPORT double FrameWorkVersionNumber;
+
+//! Project version string for FrameWork.
+FOUNDATION_EXPORT const unsigned char FrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWork/PublicHeader.h>
+
+
